3863ef4655cf2b1d9c83dbf153d43003db057d31
buntoss@gmail.com <buntoss@gmail.com>
2021-05-15 07:14:26 +1000

    step added to handle launch browser popup

--- task.py
+++ task.py
@@ -7,29 +7,42 @@ import time
 import os
 
 # variables
-browser = Browser.Browser()
 url = "https://robotsparebinindustries.com/#/robot-order" #"https://usyd.starrezhousing.com/StarRezWeb/"
 
 def download_order_file():
-    print("___attemting to download order file___")
-    browser.new_context(acceptDownloads=True)
-    browser.new_page()
-    order_file_download = browser.download("https://robotsparebinindustries.com/orders.csv")
-    os.replace(order_file_download, ".\\orderFile.csv")
-    browser.close_browser()
-    print("___order file downloaded___")
-    print()
-    return(order_file_download)
+    try:
+        browser = Browser.Browser()
+        print("___attemting to download order file___")
+        browser.new_context(acceptDownloads=True)
+        browser.new_page()
+        order_file_download = browser.download("https://robotsparebinindustries.com/orders.csv")
+        os.replace(order_file_download, ".\\orderFile.csv")
+    finally:
+        browser.close_browser()
+        print("___order file downloaded___")
+        print()
+        return(order_file_download)
+
 
 def confirm_constitution_response():
     dialogs = Dialogs()
     dialogs.create_form()
-    dialogs.add_text_input(label="Let us know your thoughts?", name="hello world")
-    dialogs.request_response()
+    dialogs.add_title(title="Confirmation of Constitutional Rights")
+    dialogs.add_text(value="Before we continue, you must first confirm the below with regards to the order form that the bot completes.")
+    dialogs.add_dropdown(element_id="dropdown_selected", label="BY USING THIS ORDER FORM, I GIVE UP ALL MY CONSTITUTIONAL RIGHTS FOR THE BENEFIT OF ROBOTSPAREBIN INDUSTRIES INC.", options=["OK", "Yep", "I guess so...", "No way!"])
+    constitution_response = dialogs.request_response()
+    return(constitution_response)
 
+def open_and_complete_form(url: str, constitutional_response: str):
+    try:
+        browser = Browser.Browser()
+        browser.open_browser(url)
+        button_response = "text=" + constitutional_response
+        browser.click(selector=button_response)
+        time.sleep(2)
+    finally:
+        browser.close_browser()
 
-def open_the_website(url: str):
-    browser.open_browser(url)
 
 def complete_form():
     try:
@@ -40,6 +53,7 @@ def complete_form():
         companyRole = "Developer"
         firstName = "Tylor"
         company = "CA Labs"
+        browser = Browser.Browser()
         browser.fill_text('xpath=//input[@ng-reflect-name="labelAddress"]', address)
         browser.fill_text('xpath=//input[@ng-reflect-name="labelLastName"]', lastName)
         browser.fill_text('xpath=//input[@ng-reflect-name="labelEmail"]', email)
@@ -57,17 +71,17 @@ def complete_form():
 
 
 def end_session():
-    browser.close_page()
-    nothing = 1 + 1
+    print("Goodbye world")
 
 if __name__ == "__main__":
     try:
         print('STARTED: session started')
         print()
         #download_order_file()
-        confirm_constitution_response()
-        #open_the_website(url)
-        #complete_form()
+        constitution_response = confirm_constitution_response()
+        cons_response_selected = constitution_response.get('dropdown_selected')
+        assert cons_response_selected != "No way!", "Unable to continue as user selected 'No way!' on constitution form."
+        open_and_complete_form(url, constitutional_response=cons_response_selected)
         print()
         print("COMPLETED: all tasks completed")
     except Exception as err:
